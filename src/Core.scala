40ebdda1aef8953673267f9994fb3bbcfadd742b
Konstantin Levinski <konstantin.levinski@gmail.com>
2010-07-25 03:48:45 +0800

    Function trait

--- src/Core.scala
+++ src/Core.scala
@@ -5,13 +5,44 @@ case class V(x:Double, y:Double, z:Double){
 	import Math._
 	 
 	def length() = sqrt(x*x+y*y+z*z)
+	def normalized() = this/this.length()
+	def inversed() = V(-x, -y, -z);
 	def lengthSquared() = x*x+y*y+z*z
 	def +(that:V) = V(x+that.x, y+that.y, z+that.z)
 	def -(that:V) = V(x-that.x, y-that.y, z-that.z)
 	def *(times:Double) = V(x*times,y*times,z*times);
+	def /(times:Double) = V(x/times,y/times,z/times);
 	def *(that:V) = x*that.x + y*that.y + z*that.z;
 	def ^(that:V) = V(y*that.z-z*that.y,
 					  z*that.x-x*that.z,
 					  x*that.y-y*that.x);
+	def midpoint(that:V) = V( (x+that.x)/2, (y+that.y)/2, (z+that.z)/2);
+	override def toString() = "(" + x.toString + "," + y.toString + "," +z.toString + ")"
 }
- 
+
+trait Function{
+	case class F(r:V, f:Double) //Function point
+	val E = 0.0001
+
+	def apply(r:V):Double;
+	def point(r:V):F = F(r, this(r)) //Convenience, returns point+value.
+	def find(a:V, b:V):Option[V] = {
+		def findHelper(a:F, b:F, dist:Double ):Option[V] = {
+			if (dist < E) return Some( a.r.midpoint(b.r) )
+			if ( a.f * b.f > 0 ) return None // No NaN / +Inf...
+			val m = this.point(a.r.midpoint(b.r));
+			findHelper( if ( a.f * m.f < 0 ) a else b , m, dist/2);
+		}
+		findHelper(this.point(a), this.point(b), (a-b).length())
+	}
+	def grad(r:V):V = {
+		val dx = this( r+V(E,0,0) ) - this( r-V(E,0,0) );
+		val dy = this( r+V(0,E,0) ) - this( r-V(0,E,0) );
+		val dz = this( r+V(0,0,E) ) - this( r-V(0,0,E) );
+		V(dx/(2*E),dy/(2*E),dz/(2*E))
+	}
+}
+
+class Cell(zero:V, unit:V) {
+    def subdivide = {}
+}
--- src/CoreTest.scala
+++ src/CoreTest.scala
@@ -29,6 +29,12 @@ class VTest {
 	val e2 = V(0,1,0);
 	val e3 = V(0,0,1);
 
+	class Sphere(radius:Double) extends Function {
+		def apply(r:V) = radius-r.length;
+	}
+	
+	val s = new Sphere(1);
+	
 	//near, length
 	@Test def equality() = {
 		assertTrue( V(0,0,0).lengthSquared < 0.1  )
@@ -40,10 +46,14 @@ class VTest {
    @Test def basics() = {
 	  same( 3.0, l.length())
 	  same( a+a , aa)
+	  same( a.inversed(), a*(-1.0))
 	  same( aa-a, a)
 	  same( (a-a).length(), 0.0)
 	  same( aa, a*2)
 	  same( aa*aa, aa.lengthSquared())
+	  same( a.midpoint(b) , (a+b)*0.5 )
+	  same( aa.normalized() , aa / aa.length() )
+	  same( a*3.0 , a/(1.0/3.0) )
   }
    
    // ^
@@ -55,5 +65,16 @@ class VTest {
 	  same( cross*l, 0.0 )
 	  same( cross*a, 0.0 )
 	  same( e1 ^ e2, e3)
+   }  
+   
+   @Test def function() = {
+	  same( s(o), 1)
+	  same( s(a.normalized()), 0)
+	  ( s.find(l / 4, a)) match {
+	 	  case None => assertTrue(false)
+	 	  case Some(x) => same(x.length, 1 ) };
+	  assertTrue( ( s.find(a, b) ) match { case None=>true 
+	 	  	                               case Some(_)=>false }  )
+	  same ( s.grad(a), a.normalized().inversed() )
    }  
 }
